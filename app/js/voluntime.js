1deea01b01d65ae8d0166147d4eda126a9126085
Brian Chang <eviltoylet@hotmail.com>
2014-09-29 08:40:41 -0700

    Add fake data and a draggable volunteer bank

--- app/js/voluntime.js
+++ app/js/voluntime.js
@@ -1,6 +1,27 @@
 'use strict';
 
 (function () {
+  var getVolunteerShifts = function () {
+    var date = new Date();
+    var month = date.getMonth();
+    var day = date.getDate();
+    var year = date.getFullYear();
+    
+    var schedule = [
+      {title: 'Joe Smith', start: new Date(year, month, day, 11), end: new Date(year, month, day, 12), allDay: false},
+      {title: 'Jill Smith', start: new Date(year, month, day, 11), end: new Date(year, month, day, 12), allDay: false},
+      {title: 'Lana Lee', start: new Date(year, month, day, 12), end: new Date(year, month, day, 14), allDay: false},
+      {title: 'Jerry Scott', start: new Date(year, month, day, 7), end: new Date(year, month, day, 18), allDay: false},
+      {title: 'Jack Bauer', start: new Date(year, month, day, 16), end: new Date(year, month, day, 17), allDay: false},
+      {title: 'Jenny Jones', start: new Date(year, month, day, 17), end: new Date(year, month, day, 18), allDay: false},
+      {title: 'Garret Ashton', start: new Date(year, month, day, 7), end: new Date(year, month, day, 8), allDay: false},
+      {title: 'Lucy Barm', start: new Date(year, month, day, 9), end: new Date(year, month, day, 12), allDay: false},
+      {title: 'Lenny Bernstein', start: new Date(year, month, day, 18), end: new Date(year, month, day, 19), allDay: false}
+    ];
+    
+    return schedule;
+  };
+  
   var voluntime = angular.module('voluntime', ['ngRoute', 'ui.calendar', 'snap']);
   voluntime.config(function($routeProvider, snapRemoteProvider){
     $routeProvider
@@ -25,8 +46,9 @@
     $scope.switchToDayView = function(date, event, view) {
       $scope.changeView(date, 'agendaDay');
     };
-
-    $scope.eventSources = [];
+    
+    $scope.events = getVolunteerShifts();
+    $scope.eventSources = [$scope.events];
     $scope.calendarConfig = {
       dayClick: $scope.switchToDayView,
       header: {
