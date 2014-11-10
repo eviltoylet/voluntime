20e10c3f41ace87c00e08690edc9b0775dbb26ab
Brian Chang <eviltoylet@hotmail.com>
2014-11-10 08:42:25 -0800

    Add a way to update existing users

--- app/routes.js
+++ app/routes.js
@@ -2,24 +2,29 @@
 
 var Profile = require('./models/profile');
 var UserWrapper = require('./wrapper/userWrapper');
-module.exports = function(app) {
-  app.get('/api/profiles', function(req, res) {
-    Profile.find(function(err, profiles) {
+module.exports = function (app) {
+  app.get('/api/profiles', function (req, res) {
+    Profile.find(function (err, profiles) {
       if (err) {
         res.send(err);
       }
-      
+
       res.json(profiles);
     });
   });
-  
-  app.post('/api/users', function(req, res) {
+
+  app.post('/api/users', function (req, res) {
     // TODO: Figure out how I can do input validation?
-    var user = UserWrapper.wrapExternal(req.body);
+    var user = UserWrapper.wrapExternal(req);
     user.save(res);
   });
-  
-  app.get('*', function(req, res) {
+
+  app.put('/api/users/:email', function (req, res) {
+    var user = UserWrapper.wrapExternal(req);
+    user.update(res);
+  });
+
+  app.get('*', function (req, res) {
     res.sendfile('./public/views/index.html');
   });
 };
--- app/wrapper/userWrapper.js
+++ app/wrapper/userWrapper.js
@@ -2,54 +2,105 @@ var User = require('../models/user');
 var bcrypt = require('bcrypt');
 var SALT_WORK_FACTOR = 10;
 
-var findExisting = function(emailAddress, foundCallback, notFoundCallback) {
-  var existingQuery = User.where({emailAddress: emailAddress});
-  existingQuery.findOne(function(err, user) {
+var findExisting = function (emailAddress, foundCallback, notFoundCallback) {
+  var existingQuery = User.where({
+    emailAddress: emailAddress
+  });
+  existingQuery.findOne(function (err, user) {
     if (user == null) {
-      notFoundCallback(new User({emailAddress: emailAddress}));
+      notFoundCallback(new User({
+        emailAddress: emailAddress
+      }));
     } else {
       foundCallback(user);
     }
   });
 }
 
-module.exports.wrapInternal = function(input) {
-  
+module.exports.wrapInternal = function (input) {
+
 };
 
-module.exports.wrapExternal = function(input) {
+module.exports.wrapExternal = function (request) {
+  var inputUser = request.body;
   var save = function (response) {
-    findExisting(input.emailAddress,
-    function existingUser(user) {
-      response.status(409).send({message: "A user with e-mail address " + input.emailAddress + " already exists."});
-    },
-    function createUser(user) {
-      bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-      if (err) {
-        console.error(err);
-        response.status(500).send();
-      }
-      
-      bcrypt.hash(input.password, salt, function(err, hash) {
-        if (err) {
-          console.error(err);
-          response.status(500).send();
-        }
-        user.encryptedPassword = hash;
-        user.save(function(err, user) {
+    findExisting(inputUser.emailAddress,
+      function existingUser(user) {
+        response.status(409).send({
+          message: "A user with e-mail address " + user.emailAddress + " already exists."
+        });
+      },
+      function createUser(user) {
+        bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+          if (err) {
+            console.error(err);
+            response.status(500).send();
+          }
+
+          bcrypt.hash(inputUser.password, salt, function (err, hash) {
+            if (err) {
+              console.error(err);
+              response.status(500).send();
+            } else {
+              user.encryptedPassword = hash;
+              user.save(function (err, user) {
+                if (err) {
+                  console.error(err);
+                  response.status(500).send();
+                } else {
+                  response.status(200).send();
+                }
+              });
+            }
+          });
+        });
+      });
+  };
+
+  var update = function (response) {
+    var inputEmail = request.params.email;
+    var inputUser = request.body;
+    findExisting(inputEmail,
+      function existingUser(user) {
+        bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
           if (err) {
             console.error(err);
             response.status(500).send();
           } else {
-            response.status(200).send(user);
+            bcrypt.hash(inputUser.password, salt, function (err, hash) {
+              if (err) {
+                console.error(err);
+                response.status(500).send();
+              }
+              user.emailAddress = inputUser.emailAddress;
+              user.encryptedPassword = hash;
+              user.save(function (err, user) {
+                if (err) {
+                  console.error(err);
+                  if (err.code == 11000) {
+                    response.status(409).send({
+                      message: "There is already a user with the e-mail address " + inputUser.emailAddress + ". Try a different one."
+                    });
+                  } else {
+                    response.status(500).send();
+                  }
+                } else {
+                  response.status(200).send();
+                }
+              });
+            });
           }
         });
+      },
+      function newUser(user) {
+        response.status(404).send({
+          message: "No user with the e-mail address " + user.emailAddress + ". Could not update."
+        });
       });
-    });
-    });
   };
-  
+
   return {
-    save: save
+    save: save,
+    update: update
   };
 };
