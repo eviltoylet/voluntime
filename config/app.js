f779daa30dada58eb8bf3ed3a1a2e1188ce350c8
Brian Chang <eviltoylet@hotmail.com>
2014-11-19 09:04:19 -0800

    Add some configuration

---
+++ config/app.js
@@ -0,0 +1,4 @@
+module.exports = {
+  port: 8000,
+  cookieMaxAge: (1000 * 60 * 60 * 24 * 365)
+};
--- server.js
+++ server.js
@@ -1,49 +1,54 @@
 // server.js
 
 // modules =================================================
-var express        = require('express');
-var app            = express();
-var bodyParser     = require('body-parser');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-var mongoose       = require('mongoose');
+var mongoose = require('mongoose');
 
 // configuration ===========================================
-    
+
 // config files
-var db = require('./config/db');
+var dbConfig = require('./config/db');
+var appConfig = require('./config/app');
+var secretsConfig = require('./config/secrets');
 
 // set our port
-var port = process.env.PORT || 8080; 
+var port = process.env.PORT || appConfig.port;
 
 // connect to our mongoDB database 
-// (uncomment after you enter in your own credentials in config/db.js)
-mongoose.connect(db.url); 
+mongoose.connect(dbConfig.url);
 
 // get all data/stuff of the body (POST) parameters
 // parse application/json 
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
 // parse application/vnd.api+json as json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+app.use(bodyParser.json({
+  type: 'application/vnd.api+json'
+}));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use(methodOverride('X-HTTP-Method-Override')); 
+app.use(methodOverride('X-HTTP-Method-Override'));
 
 // set the static files location /public/img will be /img for users
-app.use(express.static(__dirname + '/public')); 
+app.use(express.static(__dirname + '/public'));
 
 // routes ==================================================
 require('./app/routes')(app); // configure our routes
 
 // start app ===============================================
 // startup our app at http://localhost:8080
-app.listen(port);               
+app.listen(port);
 
 // shoutout to the user                     
 console.log('Magic happens on port ' + port);
 
 // expose app           
-exports = module.exports = app;            
+exports = module.exports = app;
